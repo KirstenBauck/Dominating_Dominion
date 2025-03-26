5dc1b76f26678ee51ef5b265887dc785ca269d3c
Kirsten <Khbauck@gmail.com>
2025-03-25 20:18:42 -0700

    Agent can now play

--- DominionEnv.py
+++ DominionEnv.py
@@ -1,7 +1,7 @@
 import gymnasium as gym
 import numpy as np
 from gymnasium import spaces
-from external.pydominion.dominion import Piles
+from external.pydominion.dominion import Piles, Phase
 from external.pydominion.dominion.Game import Game
 from typing import Optional
 
@@ -13,7 +13,7 @@ class DominionEnv(gym.Env):
     BUY_PHASE = 1
     CLEANUP_PHASE = 2
 
-    def __init__(self, num_players=2, card_set=None):
+    def __init__(self, num_players=2, card_set=None, quiet_flag=True):
         super(DominionEnv, self).__init__()
         self.num_players = num_players
         # Properly initialize the cards in play (Base + Kingdom)
@@ -31,7 +31,8 @@ class DominionEnv(gym.Env):
             prosperity=False,
             potions = False,
             shelters = False,
-            card_path="external/pydominion/dominion/cards"
+            card_path="external/pydominion/dominion/cards",
+            quiet = quiet_flag
         )
         
         # Using a box because PPO doesn't support a dict of dicts
@@ -89,23 +90,87 @@ class DominionEnv(gym.Env):
     def _get_info(self):
         return {}
 
-    # TODO: work on this step so an agent can play
     def step(self, action):
         """Take an action in the game (buy a card, play an action, etc.)."""
         if self.game.game_over:
             return self._get_observation(), 0, True, False, {}
         
-        # Perform a turn
-        self.game.turn()
-        
+        # Perform a turn (code from dwagon --> self.game.turn())
+        self.game._validate_cards()
+        self.game.current_player = self.game.player_to_left(self.game.current_player)
+        self.game.current_player.start_turn()
+
+        # Now play the turn
+        self.game.current_player.turn_number += 1
+        if self.game.current_player.skip_turn:
+            self.game.current_player.skip_turn = False
+            return
+        self.game.current_player.phase = Phase.ACTION
+        # The loop is so cards can change player phases
+        while True:
+            self.game.current_player._card_check()
+            match self.game.current_player.phase:
+                case Phase.ACTION:
+                    self.action_phase()
+                    if self.game.current_player.phase == Phase.ACTION:
+                        self.game.current_player.phase = Phase.BUY
+                case Phase.BUY:
+                    self.buy_phase()
+                    if self.game.current_player.phase == Phase.BUY:
+                        self.game.current_player.phase = Phase.CLEANUP
+                # Note: for now, not implementing NIGHT phase
+                case Phase.CLEANUP:
+                    self.game.current_player.cleanup_phase()
+                    break
+        self.game.current_player._card_check()
+
+        # End the turn (code from dwagon)
+        self.game.current_player.end_turn()
+        self.game._validate_cards()
+        self.game._turns.append(self.game.current_player.uuid)
+        if self.game.isGameOver():
+            self.game.game_over = True
+            for plr in self.game.player_list():
+                plr.game.game_over()
+
         # Check if the game is over
         terminated = self.game.game_over
 
         # A simple reward concerning if won the game
-        # TODO: Expand upon this reward function (not a huge problem)
+        # TODO: Expand upon this reward function
         reward = 1 if terminated and self._who_won() else 0
         
         return self._get_observation(), reward, terminated, False, self._get_info()
+
+######################## Helper Functions ###################################################
+
+    def action_phase(self) -> None:
+        #self.game.current_player.output("************ Action Phase ************")
+        while True:
+            # options is a list of dict (each dict is an option class)
+            options = self.game.current_player._choice_selection()
+
+            # This is the bot implementation right now
+            action = len(options) - 1
+            opt = options[action]
+
+            self.game.current_player._perform_action(opt)
+            if opt["action"] == "quit":
+                return
+    
+    def buy_phase(self) -> None:
+        self.game.current_player.hook_pre_buy()
+        while True:
+            options = self.game.current_player._choice_selection()
+
+            # This is the bot implementation right now
+            action = len(options) - 1
+            opt = options[action]
+
+            self.game.current_player._perform_action(opt)
+            if opt["action"] == "quit":
+                break
+        self.game.current_player.hook_end_buy_phase()
 
     def _who_won(self):
         """Determine if Player 0 won"""\
@@ -117,6 +182,7 @@ class DominionEnv(gym.Env):
         max_score = max(scores.values())
         return player_0_score == max_score
 
+    # This function isn't really necessary I dont think
     def render(self, mode="human"):
         """Print game state for debugging"""
         self.game.print_state(card_dump=True)
